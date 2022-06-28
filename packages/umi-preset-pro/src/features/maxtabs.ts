6e8af6d75f2b6e7df640a5bf0f2cd3b23c813f78
xiaohuoni <chen.xiaocong2@iwhalecloud.com>
2022-06-28 22:21:48 +0800

    chore: bug demo

---
+++ packages/umi-preset-pro/src/features/maxtabs.ts
@@ -0,0 +1,53 @@
+import { join } from 'path';
+import { IApi } from 'umi';
+
+export function winPath(path: string) {
+  const isExtendedLengthPath = /^\\\\\?\\/.test(path);
+  if (isExtendedLengthPath) {
+    return path;
+  }
+  return path.replace(/\\/g, '/');
+}
+
+export function withTmpPath(opts: {
+  api: IApi;
+  path: string;
+  noPluginDir?: boolean;
+}) {
+  return winPath(
+    join(
+      opts.api.paths.absTmpPath,
+      opts.api.plugin.key && !opts.noPluginDir
+        ? `plugin-${opts.api.plugin.key}`
+        : '',
+      opts.path,
+    ),
+  );
+}
+
+export default (api: IApi) => {
+  api.onGenerateFiles(() => {
+    api.writeTmpFile({
+      path: 'Layout.tsx',
+      content: `
+      import { useKeepOutlets } from '@umijs/max';
+
+      const Layout = () => {
+        const element = useKeepOutlets();
+        return <>{element}</>;
+      };
+      
+      export default Layout;
+`,
+    });
+  });
+
+  api.addLayouts(() => {
+    return [
+      {
+        id: 'max-tabs',
+        file: withTmpPath({ api, path: 'Layout.tsx' }),
+      },
+    ];
+  });
+};
