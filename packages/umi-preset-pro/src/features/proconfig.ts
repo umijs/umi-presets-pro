eb9c719041ac23ecb4d0dd35f02ab5b2724a3656
Guan <46285865+guansss@users.noreply.github.com>
2022-12-08 17:02:25 +0800

    fix: config being reset (#9) (#10)

--- packages/umi-preset-pro/src/features/proconfig.ts
+++ packages/umi-preset-pro/src/features/proconfig.ts
@@ -3,18 +3,16 @@ import type { IApi } from 'umi';
 export default (api: IApi) => {
   const { REACT_APP_ENV } = process.env;
 
-  const configDefaults: Record<string, any> = {
-    define: {
-      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: '',
-      REACT_APP_ENV: REACT_APP_ENV || false,
-    },
-    ...api.userConfig,
+  const define = {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: '',
+    REACT_APP_ENV: REACT_APP_ENV || false,
   };
 
   api.modifyConfig((memo: any) => {
-    Object.keys(configDefaults).forEach((key) => {
-      memo[key] = configDefaults[key];
-    });
+    memo.define = {
+      ...memo.define,
+      ...define,
+    };
     return memo;
   });
 };
