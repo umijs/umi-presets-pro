37ee30a7eb07185508a97352b9f7992cc1a17313
期贤 <qixian.cs@antgroup.com>
2022-12-08 17:04:15 +0800

    docs: add changelog

--- packages/umi-preset-pro/src/index.ts
+++ packages/umi-preset-pro/src/index.ts
@@ -2,7 +2,7 @@ import type { IApi } from 'umi';
 
 export default (api: IApi) => {
   api.onStart(() => {
-    console.log('Hello PRO');
+    console.log('😄 Hello PRO');
   });
   const plugins = [
     require.resolve('./features/proconfig'),
@@ -10,6 +10,7 @@ export default (api: IApi) => {
     require.resolve('@umijs/max-plugin-openapi'),
     require.resolve('@alita/plugins/dist/keepalive'),
     require.resolve('@alita/plugins/dist/tabs-layout'),
+    require.resolve('@umijs/request-record'),
   ];
   return {
     plugins,
